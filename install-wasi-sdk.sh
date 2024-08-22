09f5102bb23d4b56471ed95549ef0220d3c17a57
Gavin Hayes <gavin@dylibso.com>
2024-08-22 18:11:50 -0400

    ci: install wasi-sdk 24

--- install-wasi-sdk.sh
+++ install-wasi-sdk.sh
@@ -6,12 +6,13 @@ set -euo pipefail
 PATH_TO_SDK="./wasi-sdk"
 if [[ ! -d $PATH_TO_SDK ]]; then
     TMPGZ=$(mktemp)
-    VERSION_MAJOR="22"
+    VERSION_MAJOR="24"
     VERSION_MINOR="0"
+    ARCH=$(uname -m)
     if [[ "$(uname -s)" == "Darwin" ]]; then
-        curl --fail --location --silent https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${VERSION_MAJOR}/wasi-sdk-${VERSION_MAJOR}.${VERSION_MINOR}-macos.tar.gz --output $TMPGZ
+        curl --fail --location --silent https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${VERSION_MAJOR}/wasi-sdk-${VERSION_MAJOR}.${VERSION_MINOR}-${ARCH}-macos.tar.gz --output $TMPGZ
     else
-        curl --fail --location --silent https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${VERSION_MAJOR}/wasi-sdk-${VERSION_MAJOR}.${VERSION_MINOR}-linux.tar.gz --output $TMPGZ
+        curl --fail --location --silent https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${VERSION_MAJOR}/wasi-sdk-${VERSION_MAJOR}.${VERSION_MINOR}-${ARCH}-linux.tar.gz --output $TMPGZ
     fi
     mkdir $PATH_TO_SDK
     tar xf $TMPGZ -C $PATH_TO_SDK --strip-components=1
